845a8081f2f2c2e314971daf883193981f3d22b0
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-17 14:02:59 +0900

    first commit

---
+++ src/main/java/obanyan/dbnyans/DbnyansApplication.java
@@ -0,0 +1,13 @@
+package obanyan.dbnyans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbnyansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbnyansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/obanyan/dbnyans/DbnyansApplicationTests.java
@@ -0,0 +1,13 @@
+package obanyan.dbnyans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbnyansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
