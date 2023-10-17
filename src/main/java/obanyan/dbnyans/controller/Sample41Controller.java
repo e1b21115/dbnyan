290ceca87b31b521b7e24bb77065cf8fdb321c24
e1b21072 <e1b21072@oit.ac.jp>
2023-10-17 15:53:14 +0900

    DBの先頭のデータをSELECTで一つだけ取得して表示する

--- src/main/java/obanyan/dbnyans/controller/Sample41Controller.java
+++ src/main/java/obanyan/dbnyans/controller/Sample41Controller.java
@@ -1,15 +1,39 @@
 package obanyan.dbnyans.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import obanyan.dbnyans.model.Chamber;
+import obanyan.dbnyans.model.ChamberMapper;
 
 @Controller
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
   @GetMapping("step1")
   public String sample41() {
+    return "sample41.html";
+  }
+
+  /**
+   * 指定したidをPATHパラメータで受け取り，そのidに対応するデータを取得して返す
+   *
+   * @param id
+   * @param model
+   * @return
+   */
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
     return "sample41.html";
   }
 
---
+++ src/main/java/obanyan/dbnyans/model/ChamberMapper.java
@@ -0,0 +1,16 @@
+package obanyan.dbnyans.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface ChamberMapper {
+
+  @Select("SELECT id,userName,chamberName from chamber where id = #{id}")
+  Chamber selectById(int id);
+
+}
